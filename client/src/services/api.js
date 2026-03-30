18f8e35998b16971b751f8eacbd93c39df35d1db
Abdulsheik786 <abdulsheik278@gmail.com>
2026-03-30 12:08:14 +0530

    updated api url

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:5000/api";
+const BASE_URL = "https://pearmedia-backend.onrender.com/api";
 
 export const enhanceText = async (prompt) => {
   const res = await fetch(`${BASE_URL}/text/enhance`, {
